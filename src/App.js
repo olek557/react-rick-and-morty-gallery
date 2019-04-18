93fc188128eeb99a6c0e9b5152ac37f848d9183a
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2019-04-18 19:10:06 +0300

    basic styles and markup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import getData from './api'
-import './App.css';
+import React, { Component } from "react";
+import getData from "./api";
+import "./App.css";
 
-console.log('getData', getData);
+console.log("getData", getData);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="card-wrapper">
+          <div className="card">
+            <div className="card__image">
+              <img
+                src="https://rickandmortyapi.com/api/character/avatar/183.jpeg"
+                alt="rick"
+              />
+            </div>
+            <div className="card__info">
+              <div className="card__info-row">Name</div>
+              <div className="card__info-row">Name</div>
+              <div className="card__info-row">Name</div>
+              <div className="card__info-row">Name</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
